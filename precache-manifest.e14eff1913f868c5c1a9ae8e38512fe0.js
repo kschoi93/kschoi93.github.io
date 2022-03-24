6ebb91aec87b69cbe3fae1b2def6f04b42777cfa
kschoi <kyungsik0907@gmail.com>
2022-03-24 19:47:10 +0900

    Updates

---
+++ precache-manifest.e14eff1913f868c5c1a9ae8e38512fe0.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "a37e8d7ad0bcfc48e24e",
+    "url": "/varo-ent/static/css/main.e0382ed6.chunk.css"
+  },
+  {
+    "revision": "a37e8d7ad0bcfc48e24e",
+    "url": "/varo-ent/static/js/main.a37e8d7a.chunk.js"
+  },
+  {
+    "revision": "f23b7a80dcbe2fa78070",
+    "url": "/varo-ent/static/js/1.f23b7a80.chunk.js"
+  },
+  {
+    "revision": "a3637bc24e4186ab7fcb",
+    "url": "/varo-ent/static/js/runtime~main.a3637bc2.js"
+  },
+  {
+    "revision": "7b972e43f4cb5f6ad4e9356ca61b9184",
+    "url": "/varo-ent/static/media/0.7b972e43.jpg"
+  },
+  {
+    "revision": "2ac0ca6ddd69967f980d3034dbbb402f",
+    "url": "/varo-ent/static/media/1.2ac0ca6d.jpg"
+  },
+  {
+    "revision": "db85fd252cfe381388a45b7d4b90a691",
+    "url": "/varo-ent/static/media/10.db85fd25.jpg"
+  },
+  {
+    "revision": "e2326d30c4955431a3078456448b02ec",
+    "url": "/varo-ent/static/media/11.e2326d30.jpg"
+  },
+  {
+    "revision": "b6daff8cfeb9727ef1e817420d8bccc7",
+    "url": "/varo-ent/static/media/2.b6daff8c.jpg"
+  },
+  {
+    "revision": "354ce0986273c9b235da09fbe8a4b429",
+    "url": "/varo-ent/static/media/3.354ce098.jpg"
+  },
+  {
+    "revision": "b6a5554a678f4114cf0dd37adc137ad5",
+    "url": "/varo-ent/static/media/4.b6a5554a.jpg"
+  },
+  {
+    "revision": "c39139a81acf3f79bdaaa9b81703a386",
+    "url": "/varo-ent/static/media/5.c39139a8.jpg"
+  },
+  {
+    "revision": "0c09256082e6ddd253b972a65996247f",
+    "url": "/varo-ent/static/media/6.0c092560.jpg"
+  },
+  {
+    "revision": "ee8fbe0488c8499faf01affe86f79cac",
+    "url": "/varo-ent/static/media/7.ee8fbe04.jpg"
+  },
+  {
+    "revision": "e734fec175672bb535655667257baa5c",
+    "url": "/varo-ent/static/media/8.e734fec1.jpg"
+  },
+  {
+    "revision": "b48c0da1912525edb5ceb2371ae43ede",
+    "url": "/varo-ent/static/media/9.b48c0da1.jpg"
+  },
+  {
+    "revision": "bd40fbb71afb944c3da30d5ec7f3879f",
+    "url": "/varo-ent/static/media/1.bd40fbb7.jpg"
+  },
+  {
+    "revision": "5c7048e330f24f0e6b5e27059c6afb42",
+    "url": "/varo-ent/static/media/2.5c7048e3.jpg"
+  },
+  {
+    "revision": "65d6b243269feb53696010549aac36b4",
+    "url": "/varo-ent/static/media/3.65d6b243.jpg"
+  },
+  {
+    "revision": "0c09256082e6ddd253b972a65996247f",
+    "url": "/varo-ent/static/media/4.0c092560.jpg"
+  },
+  {
+    "revision": "7b972e43f4cb5f6ad4e9356ca61b9184",
+    "url": "/varo-ent/static/media/5.7b972e43.jpg"
+  },
+  {
+    "revision": "7b4b5cfe628cc191dbb9a2b680847569",
+    "url": "/varo-ent/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fdabf35f5037b8df2ae2ea823a84a453.js"
+  "/varo-ent/precache-manifest.e14eff1913f868c5c1a9ae8e38512fe0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/varo-ent/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
